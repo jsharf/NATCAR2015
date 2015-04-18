34c15776b7b1da2209804e863c46b9998d2ffada
fughilli <kbalke@ucla.edu>
2015-04-17 19:00:28 -0700

    Skeletonized camera implementation

---
+++ Camera/camera.c
@@ -0,0 +1,28 @@
+/*
+ * camera.c
+ *
+ *  Created on: Apr 17, 2015
+ *      Author: Kevin
+ */
+
+#include "camera.h"
+
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/sysctl.h"
+
+#include "inc/hw_gpio.h"
+#include "inc/hw_pwm.h"
+
+#include "driverlib/pin_map.h"
+#include "inc/hw_memmap.h"
+
+void camera_init()
+{
+
+}
+
+void camera_registerCallback(camera_callback_t callback)
+{
+
+}
---
+++ Camera/camera.h
@@ -0,0 +1,22 @@
+/*
+ * camera.h
+ *
+ *  Created on: Apr 17, 2015
+ *      Author: Kevin
+ */
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+void camera_init();
+
+typedef uint16_t camera_sample_t;
+
+// Callback type:
+// arg0 -> sample buffer
+// arg1 -> number of samples
+typedef void(*camera_callback_t)(camera_sample_t*, uint32_t);
+
+void camera_registerCallback(camera_callback_t callback);
+
+#endif /* CAMERA_H_ */
