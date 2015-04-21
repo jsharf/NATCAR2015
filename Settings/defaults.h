0a3254f42417d2283f2281ddce610bb5c04fd1e0
fughilli <kbalke@ucla.edu>
2015-04-21 15:55:34 -0700

    Added settings file

---
+++ Settings/defaults.h
@@ -0,0 +1,66 @@
+/*
+ * defaults.h
+ *
+ *  Created on: Apr 19, 2015
+ *      Author: Kevin
+ */
+
+#ifndef DEFAULTS_H_
+#define DEFAULTS_H_
+
+//#define DEFAULT_STEER_P 			(0.9f)
+//#define DEFAULT_STEER_D 			(0.01f)
+//#define DEFAULT_STEER_I 			(0.0f)
+//
+//#define DEFAULT_SPEED_MULT 			(0.006f)
+//#define DEFAULT_SPEED_CONT			(false)
+//
+//#define DEFAULT_THRESHOLD			(0.65f)
+//
+//#define DEFAULT_EDGECLIP_COUNT 		(5)
+
+// 12:40 PM:
+//#define DEFAULT_STEER_P 			(1.5f)
+//#define DEFAULT_STEER_D 			(0.07f)
+//#define DEFAULT_STEER_I 			(0.0f)
+//
+//#define DEFAULT_SPEED_MULT 			(0.005f)
+//#define DEFAULT_SPEED_CONT			(false)
+//
+//#define DEFAULT_THRESHOLD			(0.65f)
+//
+//#define DEFAULT_EDGECLIP_COUNT 		(5)
+//
+//#define DEFAULT_BE_STILL 			(true)
+
+// 1:04 PM:
+//#define DEFAULT_STEER_P 			(1.5f)
+//#define DEFAULT_STEER_D 			(0.06f)
+//#define DEFAULT_STEER_I 			(0.01f)
+//
+//#define DEFAULT_SPEED_MULT 			(0.005f)
+//#define DEFAULT_SPEED_CONT			(true)
+//
+//#define DEFAULT_THRESHOLD			(0.65f)
+//
+//#define DEFAULT_EDGECLIP_COUNT 		(5)
+//
+//#define DEFAULT_BE_STILL 			(false)
+
+// 3:46 PM:
+#define DEFAULT_STEER_P 			(2.2f)
+#define DEFAULT_STEER_D 			(0.06f)
+#define DEFAULT_STEER_I 			(0.01f)
+
+#define DEFAULT_SPEED_MULT 			(0.01f)
+#define DEFAULT_SPEED_CONT			(true)
+
+#define DEFAULT_THRESHOLD			(0.65f)
+
+#define DEFAULT_EDGECLIP_COUNT 		(5)
+
+#define DEFAULT_BE_STILL 			(false)
+
+#define DEFAULT_MIN_SPEED			(0.1f)
+
+#endif /* DEFAULTS_H_ */
